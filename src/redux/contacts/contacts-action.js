2b1a2ef7f8332921c770a2f8f5339b96b33f2dd6
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-08-26 21:56:28 +0300

    hw-07

--- src/redux/contacts/contacts-action.js
+++ src/redux/contacts/contacts-action.js
@@ -1,18 +1,6 @@
-/* eslint-disable import/no-anonymous-default-export */
-import types from './contacts-types';
 import { createAction } from '@reduxjs/toolkit';
-import { v4 as uuidv4 } from 'uuid';
 
-const addContact = createAction(types.ADD_CONTACT, (name, number) => ({
-  payload: {
-    id: uuidv4(),
-    name,
-    number,
-  },
-}));
+const addFilter = createAction('contacts/addFilter');
 
-const deleteContact = createAction(types.DELETE_CONTACT);
- const addFilter = createAction(types.ADD_FILTER);
-
-export default { addContact, deleteContact, addFilter };
+export default addFilter;
 
--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,15 +1,18 @@
-export const getItems = state => state.contacts.items;
-export const getFilter = state => state.contacts.filter;
+import memoize from 'memoizee';
 
-export const filteredContacts = state => {
+export const getItems = (state) => state.contacts.items;
+export const getFilter = (state) => state.contacts.filter;
+export const isLoading = (state) => state.contacts.loading;
+
+export const filteredContacts = memoize((state) => {
   const contacts = getItems(state);
   const filter = getFilter(state);
 
-  const getFiltredContacts = contacts => {
+  const getFiltredContacts = (contacts) => {
     const lowerCaseFilter = filter.toLowerCase();
-    return contacts.filter(person =>
-      person.name.toLowerCase().includes(lowerCaseFilter),
+    return contacts.filter((person) =>
+      person.name.toLowerCase().includes(lowerCaseFilter)
     );
   };
   return getFiltredContacts(contacts);
-};
+});
